e4972c3ad56a9e3abb9ac2004056eb9af9c1dfac
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-11 11:51:39 +0200

    update input hook

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,11 +1,19 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { ChangeEvent, ChangeEventHandler, Dispatch, SetStateAction, useState } from "react";
 
-type Result<T> = [ T, string|undefined, boolean, Dispatch<SetStateAction<T>>, Dispatch<SetStateAction<boolean>>]
+type Result<T> = [ 
+    T, 
+    string|undefined, 
+    boolean, 
+    ChangeEventHandler<HTMLInputElement>,
+    ()=> void,
+    ()=> void,
+]
 
 export default function useInput<T>(
     initialVal: T, 
     errorMessage: string, 
-    validator: (val: T) => boolean
+    validator: (val: T) => boolean,
+    transform: (val: string) => T
 ) : Result<T> {
     const [value, setValue] = useState(initialVal);
     const [touched, setTouched] = useState(false);
@@ -13,11 +21,19 @@ export default function useInput<T>(
     const invalid = validator(value);
     const error = (invalid && touched) ? errorMessage : undefined;
 
+    const touchHandler = ()=> setTouched(true)
+    const valueChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setValue(transform(e.target.value))
+    const reset = () => {
+        setTouched(false);
+        setValue(initialVal)
+    }
+
     return [
         value,
         error,
         invalid,
-        setValue,
-        setTouched
+        valueChangeHandler,
+        touchHandler,
+        reset
     ]
 }
